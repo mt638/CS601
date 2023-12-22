7ee96b185b72755514bcc95f1e18096484bbe215
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-21 20:12:43 -0500

    Update

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -6,26 +6,24 @@ const websitepath = 'https://cs-601.vercel.app/';
 test('Check SEO Title', async ({ page }) => {
   await page.goto(websitepath);
   await expect(page).toHaveTitle(/Final Project - Fettuccine Frenzy Fiesta/);
-  await page.screenshot({ path: 'screenshots/seotitle.png', fullPage: true });
 });
 
 test('Check SEO Meta Description', async ({ page }) => {
   await page.goto(websitepath);
   const metaDescription = await page.getAttribute('meta[name="description"]', 'content');
   await expect(metaDescription).not.toBe('');
-  await page.screenshot({ path: 'screenshots/seodescription.png', fullPage: true });
 });
 
 test('Check SEO Meta Keywords', async ({ page }) => {
   await page.goto(websitepath);
   const metaKeywords = await page.getAttribute('meta[name="keywords"]', 'content');
   await expect(metaKeywords).not.toBe('');
-  await page.screenshot({ path: 'screenshots/SEOkeywords.png', fullPage: true });
 });
 
 
 test('Check SignUp Page', async ({ page }) => {
   await page.goto('websitepath');
-  await page.click('text=SIGNUP');
-  await page.screenshot({ path: 'screenshots/signup-page.png', fullPage: true });
+  await page.waitForSelector('nav');
+  await page.click('a:has-text("SIGNUP")');
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 });
