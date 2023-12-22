1c463be25050dcbded4ec5bd431a5ab75bfea67e
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-21 19:57:45 -0500

    Update

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -6,19 +6,19 @@ const websitepath = 'https://cs-601.vercel.app/';
 test('Check SEO Title', async ({ page }) => {
   await page.goto(websitepath);
   await expect(page).toHaveTitle(/Final Project - Fettuccine Frenzy Fiesta/);
-  await page.screenshot({ path: 'screenshots/homepage.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/seotitle.png', fullPage: true });
 });
 
 test('Check SEO Meta Description', async ({ page }) => {
   await page.goto(websitepath);
   const metaDescription = await page.getAttribute('meta[name="description"]', 'content');
   await expect(metaDescription).not.toBe('');
-  await page.screenshot({ path: 'screenshots/homepage.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/seodescription.png', fullPage: true });
 });
 
 test('Check SEO Meta Keywords', async ({ page }) => {
   await page.goto(websitepath);
   const metaKeywords = await page.getAttribute('meta[name="keywords"]', 'content');
   await expect(metaKeywords).not.toBe('');
-  await page.screenshot({ path: 'screenshots/homepage.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/SEOkeywords.png', fullPage: true });
 });
