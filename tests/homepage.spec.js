e97bf41f540e11d3d155c099b33a57a053d60607
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-21 20:28:31 -0500

    Update

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -31,5 +31,14 @@ test('Check SignUp Page', async ({ page }) => {
   await page.waitForSelector('nav');
   await page.click('a:has-text("SIGNUP")');
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-  await page.screenshot({ path: 'after_click_home.png' });
+});
+
+test('Check Logo Present or Not', async ({ page }) => {
+  await page.goto(websitepath);
+  try {
+    await page.waitForSelector('img[alt="logo"]', { timeout: 5000 });
+    console.log('Logo is present on the page.');
+  } catch (error) {
+    console.error('Logo is not present on the page.');
+  }
 });
