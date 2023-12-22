6b00c83f287b3426fb8b77a33b0e5a3cf99c2c8a
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-21 19:45:50 -0500

    Update

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,10 +1,23 @@
 
 const {test,expect} = require('@playwright/test');
 
+const websitepath = 'https://cs-601.vercel.app/';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://cs-601.vercel.app/');
+test('Check SEO Title', async ({ page }) => {
+  await page.goto(websitepath);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create Next App/);
+});
+
+test('Check SEO Meta Description', async ({ page }) => {
+  await page.goto(websitepath);
+  const metaDescription = await page.getAttribute('meta[name="description"]', 'content');
+  await expect(metaDescription).not.toBe('');
+});
+
+test('Check SEO Meta Keywords', async ({ page }) => {
+  await page.goto(websitepath);
+  const metaKeywords = await page.getAttribute('meta[name="keywords"]', 'content');
+  await expect(metaKeywords).not.toBe('');
 });
